1c179fe18bcebde2b130fb8e12a758d0ae3ae115
Timofey Bobkov <bobkov.tim@gmail.com>
2020-01-23 19:24:53 +0300

    fix

--- lib/create_network.sh
+++ lib/create_network.sh
@@ -8,7 +8,8 @@ function generateKeyPair(){
     #echo -ne "\n" | constellation-node --generatekeys=${mNode} 1>>/dev/null
     #echo -ne "\n" | constellation-node --generatekeys=${mNode}a 1>>/dev/null
     #mv ${mNode}*.*  ${mNode}/node/keys/.
-    java -jar /tessera/tessera-app.jar -keygen -filename ${mNode}/node/keys/${mNode},${mNode}/node/keys/${mNode}a < /dev/null
+    java -jar /tessera/tessera-app.jar -keygen -filename ${mNode}/node/keys/${mNode} < /dev/null 1>>/dev/null
+    java -jar /tessera/tessera-app.jar -keygen -filename ${mNode}/node/keys/${mNode}a < /dev/null 1>>/dev/null
 }
 
 #function to create node initialization script
